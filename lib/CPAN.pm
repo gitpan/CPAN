2a764576697999bc18d514a4225378baed68f7fe
Andreas J. König <andreas.koenig.7os6vvqr@franz.ak.mind.de>
2006-08-24 05:57:41 +0000

    Import of ANDK/CPAN-1.87_56 from CPAN.
    gitpan-cpan-distribution: CPAN
    gitpan-cpan-version:      1.87_56
    gitpan-cpan-path:         ANDK/CPAN-1.87_56.tar.gz
    gitpan-cpan-author:       ANDK
    gitpan-cpan-maturity:     developer

--- lib/CPAN.pm
+++ lib/CPAN.pm
@@ -1,6 +1,6 @@
 # -*- Mode: cperl; coding: utf-8; cperl-indent-level: 4 -*-
 package CPAN;
-$VERSION = '1.87_55';
+$VERSION = '1.87_56';
 $VERSION = eval $VERSION;
 use strict;
 
@@ -135,6 +135,7 @@ sub shell {
         }}
 	# $term->OUT is autoflushed anyway
         for ($CPAN::Config->{term_ornaments}) {
+            local $Term::ReadLine::termcap_nowarn = 1;
             $term->ornaments($_) if defined;
         }
 	my $odef = select STDERR;
@@ -7059,7 +7060,7 @@ Batch mode:
   install $distro;                                # same thing
   CPAN::Shell->install($distro);                  # same thing
   CPAN::Shell->expandany($distro)->install;       # same thing
-  CPAN::Shell->expand("Module",$distro)->install; # same thing
+  CPAN::Shell->expand("Distribution",$distro)->install; # same thing
 
 =head1 STATUS
 
@@ -7596,6 +7597,8 @@ yet been run, it will be run first. A C<make test> will be issued in
 any case and if this fails, the install will be canceled. The
 cancellation can be avoided by letting C<force> run the C<install> for
 you.
+
+Note that install() gives no meaningful return value. See uptodate().
 
 =item CPAN::Distribution::isa_perl()
 
